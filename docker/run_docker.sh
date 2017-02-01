81af282807d769d43d06aac360e8ce4493787d07
Frank Dekervel <frank@kapernikov.com>
2017-02-01 11:54:46 +0100

    update dockerfile to be able to run notebooks again

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,4 +1,4 @@
 export parent_path=$(readlink -f ../)
 
-docker run -it --rm -v $parent_path:/opt/source ros_with_python /bin/sh -c 'cd /opt/source/ros_catkin_ws; /opt/ros/kinetic/env.sh bash'
+docker run -it --rm -p 8888:8888 -v $parent_path:/opt/source ros_with_python /bin/sh -c 'cd /opt/source/ros_catkin_ws; /opt/ros/kinetic/env.sh bash'
 
