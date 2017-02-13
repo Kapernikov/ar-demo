6c555bf72b24cdbad419315c7268a7ce4fa21687
Frank Dekervel <frank@kapernikov.com>
2017-02-13 14:57:49 +0100

    docker compose setup

---
+++ docker/compose.sh
@@ -0,0 +1,12 @@
+export parent_path=$(cd "../" 2>/dev/null && pwd -P)
+
+xhost +
+
+export sourcedir=$parent_path
+export webdir=$parent_path/blender
+docker-compose build
+
+docker-compose kill
+docker-compose up
+
+
